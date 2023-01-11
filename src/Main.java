e6a716992155f6c2892fd924ceb378b391349b0b
Alma Caciula Negrea <almacaciulanegrea@gmail.com>
2023-01-11 14:17:21 +0200

    Completamento esercizio For Loop 01

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+public class Main {
+
+    public static void main(String[] args) {
+
+        /*
+        Ho crato il for loop, usando una variabile i uguale a 5,
+        poi ho aggiunto che i fosse piu piccolo di 5, e poi
+        ho fatto in modo che dopo ogni stampa del codice
+        si aggiungesse 1 a i.
+         */
+        for(int i = 5; i <= 25; i++){
+            System.out.println(i);
+        }
+    }
+}
